a73ecfe445c0ae5a94f57682d91f69d1b568ffb8
chet013 <chet_013.27@mail.ru>
2024-02-20 13:14:35 +0400

    Delete Materiail UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import { Header } from './componets/Header/Header';
 import { Main } from './componets/Main/Main'
-import Box from '@mui/material/Box';
 
 function App() {
   return (
-    <Box style={{ backgroundColor: 'lightgrey', height: '100vh' }}>
-      <Header />
-      <Main />
-    </Box>
-
+    <>
+    </>
   );
 }
 
