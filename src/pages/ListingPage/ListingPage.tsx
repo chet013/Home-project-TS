3e58be24220f2cae31059cc4ab8b4ff09fbf2784
chet013 <chet_013.27@mail.ru>
2024-02-27 18:10:06 +0400

    Add loader, add empty processing, add error processing

--- src/pages/ListingPage/ListingPage.tsx
+++ src/pages/ListingPage/ListingPage.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import './style.css';
+import { Spin, Empty, Alert } from 'antd';
 import { PostCard } from '../../componets/PostCard/PostCard';
 import { Post } from '../../types';
 
 type ListingPageProps = {
     posts: Post[];
+    loading: boolean;
+    error: boolean;
 }
 
-export const ListingPage = ({ posts }: ListingPageProps) => {
+export const ListingPage = ({ posts, loading, error }: ListingPageProps) => {
+
+    if (loading) {
+        return (
+            <Spin
+                fullscreen
+                size="large"
+                tip="looking on the shelf..." />
+        )
+    } else if (posts.length === 0) {
+        return <Empty className='empty' />
+    } else if (error) {
+        <Alert message="Error Text" type="error" />
+    }
+
     return (
         <div className='page'>
             {posts.map((post) => {
@@ -24,4 +41,7 @@ export const ListingPage = ({ posts }: ListingPageProps) => {
             )}
         </div>
     );
+
+
+
 };
--- src/pages/ListingPage/ListingPageContainer.tsx
+++ src/pages/ListingPage/ListingPageContainer.tsx
@@ -3,16 +3,35 @@ import { ListingPage } from './ListingPage';
 import { Post } from '../../types';
 import { getPosts } from '../../api/getPosts';
 
+
 export const ListingPageContainer = () => {
+    const [loading, setLoading] = useState(false);
+    const [posts, setPosts] = useState<Post[]>([]);
+    const [error, setError] = useState(false)
+
     useEffect(() => {
+        setLoading(true);
         async function fetchData() {
             const postData = await getPosts();
             setPosts(postData)
+            setLoading(false);
         }
-        fetchData();
+        try {
+            setTimeout(() =>
+                fetchData(), 1000 /* проверка работы спинера */
+            );
+
+        } catch (err) {
+            setError(true)
+            setLoading(false);
+        }
     }, [])
 
-    const [posts, setPosts] = useState<Post[]>([]);
 
-    return <ListingPage posts={posts} />;
+    return <ListingPage
+        posts={posts}
+        loading={loading}
+        error={error}
+
+    />;
 };
